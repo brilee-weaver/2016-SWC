8f3e41b4427815a401acb2f74b9e6ada9e0faf48
brilee-weaver <weaver.br@husky.neu.edu>
2016-10-05 11:59:52 -0400

    grid

--- ps-04/grid.js
+++ ps-04/grid.js
@@ -1,13 +1,7 @@
-var canvas = document.getElementById('myGrid');
-var drawingPad = canvas.getContext('2d')
+function draw() {
+    var canvas = document.getElementById('myGrid');
+  if (canvas.getContext) {
+      var drawingPad = canvas.getContext('2d');
 
-console.log(canvas);
-console.log(drawingPad);
-
-var x=0
-var x=0
-
-for (var x=0; x<=5; x++) {
-for (var y=0; y<=5; y++) {
-    drawingPad.fillRect (20%,20%,30,30)
+    drawingPad.fillRect (20%,5%,30,30)
     drawingPad.fillStyle = "hsla(0,0%,50%,1)";
