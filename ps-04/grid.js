158985f99da4703cc9a98fc8943d4e32fb83ee9c
brilee-weaver <weaver.br@husky.neu.edu>
2016-10-05 11:37:44 -0400

    change tilt, size, margin

--- ps-04/grid.js
+++ ps-04/grid.js
@@ -9,5 +9,5 @@ var x=0
 
 for (var x=0; x<=5; x++) {
 for (var y=0; y<=5; y++) {
-    drawingPad.fillRect (10%,10%,30,30)
+    drawingPad.fillRect (20%,20%,30,30)
     drawingPad.fillStyle = "hsla(0,0%,50%,1)";
