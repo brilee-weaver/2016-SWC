c0365e4e3d8e471309bd1489550177202b6e811b
brilee-weaver <weaver.br@husky.neu.edu>
2016-10-05 11:34:51 -0400

    grid attempt

---
+++ ps-04/grid.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('myGrid');
+var drawingPad = canvas.getContext('2d')
+
+console.log(canvas);
+console.log(drawingPad);
+
+var x=0
+var x=0
+
+for (var x=0; x<=5; x++) {
+for (var y=0; y<=5; y++) {
+    drawingPad.fillRect (10%,10%,30,30)
+    drawingPad.fillStyle = "hsla(0,0%,50%,1)";
