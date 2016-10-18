f0985c03f53496a9eaa29fe4b1207587834e22f8
brilee-weaver <weaver.br@husky.neu.edu>
2016-10-18 16:33:20 -0400

    change

--- ps-04/grid.js
+++ ps-04/grid.js
@@ -1,13 +1,13 @@
 var canvas = document.getElementById('myGrid');
-var drawingPad = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
 
-   drawingPad.fillStyle = "hsla(0,10%,10%,1)";
-   drawingPad.fillRect(0, 0, 20, 20);
+   ctx.fillStyle = "hsla(0,10%,10%,1)";
+   ctx.fillRect(0, 0, 20, 20);
 
 
   console.log(canvas);
-  console.log(drawingPad);
+  console.log(ctx);
 
    var getColor = function(hue, saturation, lightness, alpha) {
      var colorString = "hsla(" +
@@ -23,16 +23,11 @@ var drawingPad = canvas.getContext('2d');
      drawingPad.strokeStyle = getColor(0, 0, 100, 0.3);
      drawingPad.strokeRect(x * size, y * size, size, size);
    }
-
-   var size = 50;
-   var numX = 10;
-   var numY = 10;
-   var randomLightness = 0;
-
-   for(var x = 0 ; x < numX; x++) {
-     for(var y = 0 ; y < numY; y++) {
-       randomLightness = Math.random() * 100;
-       drawingPad.fillStyle = getColor(0, 0, randomLightness, 0.5);
-       drawSquare(x,y,size);
-     }
-   }
+   
+      for(var x = 0 ; x < numX; x++) {
+        for(var y = 0 ; y < numY; y++) {
+          randomLightness = Math.random() * 100;
+          drawingPad.fillStyle = getColor(0, 0, randomLightness, 0.5);
+          drawSquare(x,y,size);
+        }
+      }
