eaeed1d0f4cf63974d3616a6628ac0084ed95447
brilee-weaver <weaver.br@husky.neu.edu>
2016-10-25 12:02:30 -0400

    change

--- ps-06/monster.js
+++ ps-06/monster.js
@@ -3,16 +3,24 @@ var ctx = canvas.getContext('2d');
 
 var brileeweaver = {};
 
+var drawImage = function(filename, ctx, x, y) {
+  var img = document.createElement("img");
+  img.src = filename;
+  img.addEventListener("load", function() {
+    ctx.drawImage(img, x, y);
+  });
+}
+
 brileeweaver.drawHead = function(ctx) {
   drawImage('//brileeweaver.github.io/2016-SWC/ps-06/MonsterHead.jpg', ctx, 0, 0);
 };
 
 brileeweaver.drawBody = function(ctx) {
-  drawImage('//brileeweaver.github.io/2016-SWC/ps-06/MonsterHead.jpg', ctx, 0, 0);
+  drawImage('//brileeweaver.github.io/2016-SWC/ps-06/MonsterBody.jpg', ctx, 0, 0);
 };
 
 brileeweaver.drawFeet = function(ctx) {
-  drawImage('//brileeweaver.github.io/2016-SWC/ps-06/MonsterHead.jpg', ctx, 0, 0);
+  drawImage('//brileeweaver.github.io/2016-SWC/ps-06/MonsterFeet.jpg', ctx, 0, 0);
 };
 
 brileeweaver.drawCorpse = function(ctx) {
