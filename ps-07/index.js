68983359e9b52bd751d757ef2b90d77e3f66c744
brilee-weaver <weaver.br@husky.neu.edu>
2016-11-01 11:39:51 -0400

    add change and light

--- ps-07/index.js
+++ ps-07/index.js
@@ -12,15 +12,36 @@ var drawImage = function(filename, ctx, x, y) {
   });
 }
 
-brileeweaver.drawPic = function(ctx) {
+brileeweaver.drawSlot = function(ctx) {
   drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/machine.png", ctx, 0, 0);
 };
 
 
-brileeweaver.drawSlot = function(ctx) {
+brileeweaver.drawMachine = function(ctx) {
   brileeweaver.drawPic(ctx);
 };
 
+brileeweaver.drawLight = function(ctx) {
+    drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/green_light.png", ctx, 250, 0);
+}
+
+brileeweaver.drawGreen = function(ctx) {
+  brileeweaver.drawLight(ctx);
+}
+
+brileeweaver.drawChange = function(ctx) {
+  drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/change.png", ctx, 180, 510);
+}
+
+brileeweaver.drawWinnings = function(ctx){
+  brileeweaver.drawChange(ctx);
+}
+
+
 window.brileeweaver = brileeweaver;
 
-window.brileeweaver.drawSlot(ctx);
+window.brileeweaver.drawMachine(ctx);
+
+window.brileeweaver.drawGreen(ctx);
+
+window.brileeweaver.drawWinnings(ctx);
