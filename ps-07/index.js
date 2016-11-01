04f7c35357b0091452bacd4427275b713c2b6676
brilee-weaver <weaver.br@husky.neu.edu>
2016-11-01 12:34:56 -0400

    add light

--- ps-07/index.js
+++ ps-07/index.js
@@ -1,7 +1,6 @@
 var canvas = document.getElementById('slot');
 var ctx = canvas.getContext('2d');
 
-
 var brileeweaver = {};
 
 var drawImage = function(filename, ctx, x, y) {
@@ -16,27 +15,31 @@ brileeweaver.drawSlot = function(ctx) {
   drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/machine.png", ctx, 0, 0);
 };
 
-
 brileeweaver.drawMachine = function(ctx) {
   brileeweaver.drawSlot(ctx);
 };
 
 brileeweaver.drawLight = function(ctx) {
-    drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/green_light.png", ctx, 240, 0);
-}
+    drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/light.png", ctx, 240, 5);
+};
 
 brileeweaver.drawGreen = function(ctx) {
   brileeweaver.drawLight(ctx);
-}
+};
 
 brileeweaver.drawChange = function(ctx) {
   drawImage("https://brileeweaver.github.io/2016-SWC/ps-07/change.png", ctx, 180, 510);
-}
+};
 
 brileeweaver.drawWinnings = function(ctx){
   brileeweaver.drawChange(ctx);
-}
+};
 
+function keyDown(evt) {
+  console.log("key down!");
+  brileeweaver.drawWinnings(ctx);
+  brileeweaver.drawChange(ctx);
+}
 
 window.brileeweaver = brileeweaver;
 
@@ -45,3 +48,5 @@ window.brileeweaver.drawMachine(ctx);
 window.brileeweaver.drawGreen(ctx);
 
 window.brileeweaver.drawWinnings(ctx);
+
+document.addEventListener("keydown", keyDown);
