5f13acd50d40880ad64530c861b15ed3e6aa2b48
brilee-weaver <weaver.br@husky.neu.edu>
2016-11-29 12:38:33 -0500

    some changes to graph

--- ps-11/graph.js
+++ ps-11/graph.js
@@ -2,9 +2,9 @@ var width = 600;
 var height = 600;
 
 var margin = {top: 40,
-							left: 40,
+							left: 60,
               right: 40,
-              bottom: 40,
+              bottom: 50,
 };
 
 var svg = d3.select("body")
@@ -14,26 +14,26 @@ var svg = d3.select("body")
 
 
   var data = [
-  	{x: 1, y: 1},
-  	{x: 2, y: 2},
-  	{x: 3, y: 4},
-  	{x: 4, y: 3},
+  	{x: 2000, y: 50456},
+  	{x: 2004, y: 62041},
+  	{x: 2008, y: 69499},
+  	{x: 2012, y: 65916},
   ];
 
   var xScale = d3.scaleBand()
-    .domain([0,5])
+    .domain([1996, 2000, 2004, 2008, 2012,2016])
     .range([margin.left, width - margin.right]);
 
   var yScale = d3.scaleLinear()
-    .domain([0,5])
+    .domain([50000, 55000, 60000, 65000, 70000])
     .range([height - margin.bottom, margin.top]);
 
   var xAxis = svg.append("g")
-    attr.("transform","translate(0,"+ (height - margin.bottom) +")")
+    .attr("transform","translate(0,"+ (height - margin.bottom) +")")
     .call(d3.axisBottom().scale(xScale));
 
   var yAxis = svg.append("g")
-    attr.("transform","translate(0,"+ margin.left) +", 0)")
+    .attr("transform","translate(" + margin.left + ", 0)")
     .call(d3.axisLeft().scale(yScale));
 
 var circles = svg.selectAll("circle")
@@ -42,4 +42,19 @@ var circles = svg.selectAll("circle")
   .append("circle")
     .attr("cx", function(d) {return xScale(d.x);})
     .attr("cy", function(d) {return yScale(d.y);})
-    .attr("r", 3);
+    .attr("r", 3)
+    .on("mouseover", biggen)
+    .on("mouseout", smallen);
+
+svg.append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("y", margin.left/3)
+  .attr("x", 0 - (height / 2))
+  .style("text-anchor", "middle")
+  .text("popular vote count");
+
+svg.append("text")
+  .attr("y", height - margin.bottom/3)
+  .attr("x", width / 2)
+  .style("text-anchor", "middle")
+  .text("election year");
